21b3db3988c38306c9303b34fba8eef69aafc023
truonghh99 <truonghh@beloit.edu>
2020-07-06 15:53:53 -0700

    Allow posting photos

--- app/src/main/java/com/example/instagram/Activities/CameraActivity.java
+++ app/src/main/java/com/example/instagram/Activities/CameraActivity.java
@@ -1,9 +1,12 @@
 package com.example.instagram.Activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -11,11 +14,18 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.instagram.R;
 import com.example.instagram.databinding.ActivityCameraBinding;
 import com.example.instagram.databinding.ActivityMainBinding;
+import com.example.instagram.models.Post;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.io.File;
 
@@ -26,7 +36,10 @@ public class CameraActivity extends AppCompatActivity {
     public final String APP_TAG = "MyCustomApp";
     public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
     public String photoFileName = "photo.jpg";
-    File photoFile;
+
+    private File photoFile;
+    private EditText etDescription;
+    private Button btShare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +54,45 @@ public class CameraActivity extends AppCompatActivity {
                 launchCamera();
             }
         });
+
+        etDescription = activityCameraBinding.etDescription;
+        btShare = activityCameraBinding.btnShare;
+
+        btShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String description = etDescription.getText().toString();
+                ParseUser user = ParseUser.getCurrentUser();
+                if (description.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "You should include a description!", Toast.LENGTH_SHORT).show();
+                }
+                if (photoFile == null) {
+                    Toast.makeText(getApplicationContext(), "You should include a photo!", Toast.LENGTH_SHORT).show();
+                }
+                savePost(description, user, photoFile);
+            }
+        });
     }
 
+    public void savePost(String description, ParseUser user, File photoFile) {
+        Log.e(TAG, "SAVING");
+        Post post = new Post();
+        post.setImg(new ParseFile(photoFile));
+        post.setDescription(description);
+        post.setUser(user);
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Error while saving image");
+                    Toast.makeText(getApplicationContext(), "Error while saving", Toast.LENGTH_SHORT).show();
+                }
+                Log.e(TAG,"Done");
+                Toast.makeText(getApplicationContext(), "Image shared", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
     public void launchCamera() {
         Log.e(TAG, "IN!");
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -64,5 +114,21 @@ public class CameraActivity extends AppCompatActivity {
 
         return new File(mediaStorageDir.getPath() + File.separator + fileName);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ivCamera.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/instagram/Activities/ParseApplication.java
+++ app/src/main/java/com/example/instagram/Activities/ParseApplication.java
@@ -2,7 +2,9 @@ package com.example.instagram.Activities;
 
 import android.app.Application;
 
+import com.example.instagram.models.Post;
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -11,6 +13,8 @@ public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
 
         // Use for troubleshooting -- remove this line for production
         Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
