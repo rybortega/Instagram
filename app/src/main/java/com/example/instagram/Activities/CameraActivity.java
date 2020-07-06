1bbf93f81673e804ee2597923d9a23ff46ff5a38
truonghh99 <truonghh@beloit.edu>
2020-07-06 15:17:36 -0700

    Add the ability to take picture

--- app/src/main/java/com/example/instagram/Activities/CameraActivity.java
+++ app/src/main/java/com/example/instagram/Activities/CameraActivity.java
@@ -21,7 +21,8 @@ import java.io.File;
 
 public class CameraActivity extends AppCompatActivity {
 
-    private ImageView ivCamera;
+    public final String TAG = "CameraActivity";
+    public ImageView ivCamera;
     public final String APP_TAG = "MyCustomApp";
     public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
     public String photoFileName = "photo.jpg";
@@ -37,37 +38,31 @@ public class CameraActivity extends AppCompatActivity {
         ivCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                launchCamera();
             }
         });
     }
 
-    public void onLaunchCamera(View view) {
+    public void launchCamera() {
+        Log.e(TAG, "IN!");
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         photoFile = getPhotoFileUri(photoFileName);
 
         Uri fileProvider = FileProvider.getUriForFile(CameraActivity.this, "com.codepath.fileprovider", photoFile);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
 
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
+        intent.resolveActivity(getPackageManager());
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
     }
 
     public File getPhotoFileUri(String fileName) {
-        // Get safe storage directory for photos
-        // Use `getExternalFilesDir` on Context to access package-specific directories.
-        // This way, we don't need to request external read/write runtime permissions.
         File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
 
-        // Create the storage directory if it does not exist
         if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
-            Log.d(APP_TAG, "failed to create directory");
+            Log.d(TAG, "failed to create directory");
         }
 
-        // Return the file target for the photo based on filename
-        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
 
-        return file;
     }
 }
